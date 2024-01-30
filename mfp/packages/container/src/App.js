bd2fb70ab10dd7ce1a75be95cc85eb4481fbf65c
UIcoder511 <umang25919998@gmail.com>
2024-01-29 21:23:18 -0500

    testing pipeline

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import MarketingApp from "./MarketingApp/MarketingApp";
 
 export default () => {
-  //
+  //test
   return (
     <>
       <MarketingApp />
