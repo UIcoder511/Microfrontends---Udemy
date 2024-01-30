d58cd06dbcbc94427c885e7a3fc76ca7c066b2a3
UIcoder511 <umang25919998@gmail.com>
2024-01-29 21:03:59 -0500

    changes, crated - CICD pipeline

---
+++ mfp/packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+const { dependencies } = require("../package.json");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
