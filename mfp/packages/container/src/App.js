cb22cec41b3f11362820c8aed4c721a662a2af7b
UIcoder511 <umang25919998@gmail.com>
2024-01-29 20:17:13 -0500

    first commit

---
+++ mfp/packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import MarketingApp from "./MarketingApp/MarketingApp";
+
+export default () => {
+  return (
+    <>
+      <MarketingApp />
+    </>
+  );
+};
